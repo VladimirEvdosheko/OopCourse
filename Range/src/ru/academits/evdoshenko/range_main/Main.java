cba80ce7f527f83f17aa70b670e543c6a815a1d9
Vladimir_Evdoshenko <evdoshenko86@gmail.com>
2022-07-15 13:31:26 +0700

    Add Range project

--- Range/src/ru/academits/evdoshenko/range_main/Main.java
+++ Range/src/ru/academits/evdoshenko/range_main/Main.java
@@ -1,7 +1,30 @@
 package ru.academits.evdoshenko.range_main;
 
+import ru.academits.evdoshenko.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Range range = new Range(0, 10);
+
+        System.out.println("Введите начало диапазона:");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.println("Введите конец диапазона:");
+        range.setTo(scanner.nextDouble());
+
+        System.out.println("Введите число:");
+        double enteredNumber = scanner.nextDouble();
+
+        System.out.printf("Начало диапазона: %f.%nКонец диапазона: %f.%nВведенное число: %f.%nДлина диапазона = %f.%n",
+                range.getFrom(), range.getTo(), enteredNumber, range.getLength());
+
+        if (range.isInside(enteredNumber)) {
+            System.out.printf("Число %f принадлежит диапазону!%n", enteredNumber);
+        } else {
+            System.out.printf("Число %f не принадлежит диапазону!%n", enteredNumber);
+        }
     }
 }
