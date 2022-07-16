11cfc07f2abd9f4b2e7586b26b18eabc2c3a5546
Vladimir_Evdoshenko <evdoshenko86@gmail.com>
2022-07-16 13:03:05 +0700

    Add Range* project

--- Range/src/ru/academits/evdoshenko/range/Range.java
+++ Range/src/ru/academits/evdoshenko/range/Range.java
@@ -32,4 +32,46 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(double beginning1, double ending1, double beginning2, double ending2) {
+        if (beginning1 == beginning2 && ending1 == ending2) {
+            return new Range(beginning2, ending2);
+        }
+
+        if (ending1 <= beginning2 || ending2 <= beginning1) {
+            return null;
+        }
+
+        return new Range(Math.max(beginning1, beginning2), Math.min(ending1, ending2));
+    }
+
+    public Range[] getUnion(double beginning1, double ending1, double beginning2, double ending2) {
+        if (beginning1 < beginning2 && ending1 >= beginning2 || beginning2 < beginning1 && ending2 >= beginning1) {
+            return new Range[]{new Range(Math.min(beginning1, beginning2), Math.max(ending1, ending2))};
+        }
+
+        return new Range[]{new Range(beginning1, ending1), new Range(beginning2, ending2)};
+    }
+
+    public Range[] getDifference(double beginning1, double ending1, double beginning2, double ending2) {
+        if (beginning1 == beginning2 && ending1 == ending2) {
+            return new Range[]{};
+        }
+
+        if (ending1 == beginning2 && beginning1 < beginning2 || ending2 == beginning1 && beginning2 < beginning1) {
+            return null;
+        }
+
+        if (beginning1 == beginning2) {
+            return new Range[]{new Range(beginning1, Math.max(ending1, ending2) - Math.min(ending1, ending2))};
+        }
+
+        if (ending1 == ending2) {
+            return new Range[]{new Range(beginning1, Math.max(beginning1, beginning2) -
+                    Math.min(beginning1, beginning2))};
+        }
+
+        return new Range[]{new Range(Math.min(beginning1, beginning2), Math.max(beginning1, beginning2)),
+                new Range(Math.min(ending1, ending2), Math.max(ending1, ending2))};
+    }
 }
--- Range/src/ru/academits/evdoshenko/range_main/Main.java
+++ Range/src/ru/academits/evdoshenko/range_main/Main.java
@@ -7,24 +7,67 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Range range = new Range(0, 10);
+        Range range1 = new Range(0, 10);
+        Range range2 = new Range(0, 10);
 
-        System.out.println("Введите начало диапазона:");
-        range.setFrom(scanner.nextDouble());
+        System.out.println("Enter the beginning of first range:");
+        range1.setFrom(scanner.nextDouble());
 
-        System.out.println("Введите конец диапазона:");
-        range.setTo(scanner.nextDouble());
+        System.out.println("Enter the ending of first range:");
+        range1.setTo(scanner.nextDouble());
 
-        System.out.println("Введите число:");
+        System.out.println("Enter the beginning of second range:");
+        range2.setFrom(scanner.nextDouble());
+
+        System.out.println("Enter the ending of first range:");
+
+        range2.setTo(scanner.nextDouble());
+
+        System.out.println("Enter your number:");
         double enteredNumber = scanner.nextDouble();
 
-        System.out.printf("Начало диапазона: %f.%nКонец диапазона: %f.%nВведенное число: %f.%nДлина диапазона = %f.%n",
-                range.getFrom(), range.getTo(), enteredNumber, range.getLength());
+        System.out.printf("Range beginning: %f.%nRange ending: %f.%nEntered number: %f.%nRange length = %f.%n",
+                range1.getFrom(), range1.getTo(), enteredNumber, range1.getLength());
 
-        if (range.isInside(enteredNumber)) {
-            System.out.printf("Число %f принадлежит диапазону!%n", enteredNumber);
+        if (range1.isInside(enteredNumber)) {
+            System.out.printf("Number %f belongs to range !%n", enteredNumber);
         } else {
-            System.out.printf("Число %f не принадлежит диапазону!%n", enteredNumber);
+            System.out.printf("Number %f does not belongs to range !%n", enteredNumber);
+        }
+
+        Range intersectionResult = range1.getIntersection(range1.getFrom(), range1.getTo(), range2.getFrom(),
+                range2.getTo());
+
+        if (intersectionResult == null) {
+            System.out.println("There are no intersection between range1 and range2!");
+        } else {
+            System.out.printf("The result of intersection between two ranges is range3: [%.3f; %.3f]%n",
+                    intersectionResult.getFrom(), intersectionResult.getTo());
+        }
+
+        Range[] unionResult = range1.getUnion(range1.getFrom(), range1.getTo(), range2.getFrom(), range2.getTo());
+
+        if (unionResult.length == 1) {
+            System.out.printf("The union result is: [%f, %f]%n", unionResult[0].getFrom(), unionResult[0].getTo());
+        } else {
+            System.out.printf("The union result are ranges: [%.3f; %.3f] and [%.3f; %.3f]%n",
+                    unionResult[0].getFrom(), unionResult[0].getTo(), unionResult[1].getFrom(), unionResult[1].getTo());
+        }
+
+        Range[] differenceResult = range1.getDifference(range1.getFrom(), range1.getTo(), range2.getFrom(),
+                range2.getTo());
+
+        if (differenceResult == null) {
+            System.out.println("It is impossible to difference.");
+        } else if (differenceResult.length == 0) {
+            System.out.println("The difference result is 0");
+        } else if (differenceResult.length == 1) {
+            System.out.printf("The difference result of two ranges without intersection is range: [%.3f; %.3f]",
+                    differenceResult[0].getFrom(), differenceResult[0].getTo());
+        } else {
+            System.out.printf("The difference result are ranges: [%.3f; %.3f] and [%.3f; %.3f]",
+                    differenceResult[0].getFrom(), differenceResult[0].getTo(), differenceResult[1].getFrom(),
+                    differenceResult[1].getTo());
         }
     }
 }
