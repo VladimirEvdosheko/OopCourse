7662cd31d79425dcceb25968a9dd760d5580fdb3
Vladimir_Evdoshenko <evdoshenko86@gmail.com>
2022-08-22 15:44:32 +0700

    /

---
+++ MyArrayList/src/academits/evdoshenko/ArrayList/MyArrayList.java
@@ -0,0 +1,280 @@
+package academits.evdoshenko.ArrayList;
+
+import java.util.*;
+
+public class MyArrayList<T> implements List<T> {
+    private int capacity;
+    private T[] items;
+    private int modCount = 0;
+
+    public MyArrayList(int capacity) {
+        //noinspection unchecked
+        items = (T[]) new Object[capacity];
+        this.capacity = 0;
+    }
+
+    public MyArrayList(T[] array) {
+        capacity = array.length;
+        items = Arrays.copyOf(array, capacity);
+    }
+
+    private void increaseCapacity() {
+        items = Arrays.copyOf(items, items.length * 2);
+    }
+
+    @Override
+    public int size() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return capacity == 0;
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return indexOf(object) >= 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyListIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(items, capacity);
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] array) {
+        //noinspection unchecked
+        return (T1[]) Arrays.copyOf(items, capacity);
+    }
+
+    @Override
+    public boolean add(T t) {
+        add(capacity, t);
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            throw new IllegalArgumentException("The object is null!");
+        }
+
+        for (int i = 0; i < capacity; i++) {
+            if (items[i].equals(o)) {
+                remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c == null) {
+            throw new IllegalArgumentException("The collection is null!");
+        }
+
+        for (Object cItem : c) {
+            if (!contains(cItem)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c == null) {
+            throw new IllegalArgumentException("The collection is null!");
+        }
+
+        for (T cItem : c) {
+            add(cItem);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (c == null) {
+            throw new IllegalArgumentException("The collection is null!");
+        }
+
+        if (index < 0 || index > items.length) {
+            throw new IllegalArgumentException("The index can't be < 0!");
+        }
+
+        for (T cItem : c) {
+            add(index, cItem);
+            index++;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        if (c == null) {
+            throw new IllegalArgumentException("The collection is null!");
+        }
+
+        for (int i = 0; i < capacity; i++) {
+            if (c.contains(items[i])) {
+                remove(i);
+                --i;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (c == null) {
+            throw new IllegalArgumentException("This collection is null!");
+        }
+
+        boolean wasRetainedAll = false;
+
+        for (int i = 0; i < capacity; i++) {
+            if (!c.contains(items[i])) {
+                remove(i);
+                wasRetainedAll = true;
+                i--;
+            }
+        }
+
+        return wasRetainedAll;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            items[i] = null;
+        }
+
+        capacity = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return items[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return items[index] = element;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index >= items.length) {
+            increaseCapacity();
+        }
+
+        items[index] = element;
+
+        ++capacity;
+        ++modCount;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= capacity) {
+            throw new IllegalArgumentException("The index is incorrect!");
+        }
+
+        items[index] = null;
+
+        //noinspection unchecked
+        T[] temp = (T[]) new Object[items.length - 1];
+
+        for (int i = 0, j = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                temp[j] = items[i];
+                j++;
+            }
+        }
+
+        items = Arrays.copyOf(temp, temp.length);
+
+        modCount++;
+        capacity--;
+
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < capacity; i++) {
+            if (items[i].equals(o)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = capacity - 1; i >= 0; i--) {
+            if (items[i].equals(o)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        //noinspection ConstantConditions
+        return null;
+    }
+
+    private class MyListIterator implements Iterator<T> {
+        private int currentIndex = -1;
+        private final int modCountCurrent = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < items.length;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("There are no next elements!");
+            }
+            if (modCountCurrent != modCount) {
+                throw new ConcurrentModificationException("There has been a list change!");
+            }
+
+            currentIndex++;
+
+            return items[currentIndex];
+        }
+    }
+}
---
+++ MyArrayList/src/academits/evdoshenko/Main/Main.java
@@ -0,0 +1,107 @@
+package academits.evdoshenko.Main;
+
+import academits.evdoshenko.ArrayList.MyArrayList;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] integerArray1 = {1, 2, 3, 4, 5};
+        Integer[] integerArray2 = {1, 2, 3, 4, 6};
+        Integer[] integerArray3 = {13, 1};
+
+        MyArrayList<Integer> numbers1 = new MyArrayList<>(integerArray1);
+        MyArrayList<Integer> numbers2 = new MyArrayList<>(integerArray2);
+        MyArrayList<Integer> numbers3 = new MyArrayList<>(integerArray1);
+        MyArrayList<Integer> numbers4 = new MyArrayList<>(integerArray2);
+        MyArrayList<Integer> numbers5 = new MyArrayList<>(integerArray3);
+        MyArrayList<Integer> numbers6 = new MyArrayList<>(integerArray1);
+        MyArrayList<Integer> numbers7 = new MyArrayList<>(integerArray2);
+        MyArrayList<Integer> numbers8 = new MyArrayList<>(5);
+
+        Iterator<Integer> iterator = numbers2.iterator();
+
+        System.out.println("Iterator work result:");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+
+        numbers1.add(7);
+        numbers1.add(8);
+        numbers1.add(9);
+        numbers1.add(10);
+        numbers1.add(11);
+
+        System.out.println();
+        System.out.println("Numbers addition result:");
+
+        for (Integer number : numbers1) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+
+        numbers1.remove(Integer.valueOf(7));
+
+        System.out.println("Removing of first integer value of 7 result:");
+        for (Integer number : numbers1) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println("Removing of all integers values of 7 result:");
+        numbers1.add(5);
+        numbers1.add(5);
+
+        numbers1.remove(5);
+        for (Integer number : numbers1) {
+            System.out.print(number + " ");
+        }
+
+        Object[] array = numbers1.toArray();
+
+        System.out.println();
+        System.out.println(Arrays.toString(array) + " ");
+
+        System.out.println("Numbers3 contains numbers4 result:");
+        //noinspection SlowListContainsAll
+        System.out.println(numbers3.containsAll(numbers4));
+
+        System.out.println("Numbers4 contains numbers3 result:");
+        //noinspection SlowListContainsAll
+        System.out.println(numbers4.containsAll(numbers3));
+
+        numbers3.addAll(numbers4);
+
+        for (Integer number : numbers3) {
+            System.out.print(number + " ");
+        }
+
+        numbers4.retainAll(numbers5);
+
+        System.out.println();
+        System.out.println("Retain result:");
+
+        for (Integer number : numbers4) {
+            System.out.print(number + " ");
+        }
+
+        numbers8.addAll(numbers1);
+
+        System.out.println();
+        System.out.println("Add all result:");
+
+        for (Integer number : numbers8) {
+            System.out.print(number + " ");
+        }
+
+        numbers7.removeAll(numbers6);
+
+        System.out.println();
+        System.out.println("Removing all result:");
+
+        for (Integer number : numbers7) {
+            System.out.print(number + " ");
+        }
+    }
+}
