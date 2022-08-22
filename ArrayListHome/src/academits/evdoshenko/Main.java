6206ba7eabfe18e52b95ccbe97d9099f08c7d0f9
Vladimir_Evdoshenko <evdoshenko86@gmail.com>
2022-08-22 15:31:22 +0700

    /

--- ArrayListHome/src/academits/evdoshenko/Main.java
+++ ArrayListHome/src/academits/evdoshenko/Main.java
@@ -9,42 +9,46 @@ public class Main {
         try (BufferedReader fileReader = new BufferedReader(new FileReader("D:\\input.txt"))) {
             String line = fileReader.readLine();
 
-            ArrayList<Integer> numbers1 = new ArrayList<>();
+            ArrayList<Integer> numbersList = new ArrayList<>();
 
             while (line != null) {
-                numbers1.add(Integer.valueOf(line));
+                numbersList.add(Integer.valueOf(line));
                 line = fileReader.readLine();
             }
 
             System.out.print("Outputting from file: ");
-            System.out.println(numbers1);
+            System.out.println(numbersList);
         } catch (FileNotFoundException var6) {
             System.out.println("The File is not exists!");
         } catch (IOException e) {
             System.out.println("Input or output error!");
         }
 
-        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16));
+        ArrayList<Integer> numbersListWithoutEvenNumbers = new ArrayList<>(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16)
+        );
 
-        for (int i = 0; i < numbers2.size(); ++i) {
-            if (numbers2.get(i) % 2 == 0) {
-                numbers2.remove(i);
+        for (int i = 0; i < numbersListWithoutEvenNumbers.size(); ++i) {
+            if (numbersListWithoutEvenNumbers.get(i) % 2 == 0) {
+                numbersListWithoutEvenNumbers.remove(i);
                 --i;
             }
         }
 
-        System.out.println("ArrayList without even numbers: " + numbers2);
+        System.out.println("ArrayList without even numbers: " + numbersListWithoutEvenNumbers);
 
-        numbers2 = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 3, 5, 5, 4, 4, 8, 11, 9, 9, 9, 15, 16));
+        numbersListWithoutEvenNumbers = new ArrayList<>(
+                Arrays.asList(1, 2, 3, 3, 3, 5, 5, 4, 4, 8, 11, 9, 9, 9, 15, 16)
+        );
 
-        ArrayList<Integer> resultNumbers = new ArrayList<>(numbers2.size());
+        ArrayList<Integer> NumbersListWithoutRepeats = new ArrayList<>(numbersListWithoutEvenNumbers.size());
 
-        for (Integer number : numbers2) {
-            if (!resultNumbers.contains(number)) {
-                resultNumbers.add(number);
+        for (Integer number : numbersListWithoutEvenNumbers) {
+            if (!NumbersListWithoutRepeats.contains(number)) {
+                NumbersListWithoutRepeats.add(number);
             }
         }
 
-        System.out.println("ArrayList without repeats: " + resultNumbers);
+        System.out.println("ArrayList without repeats: " + NumbersListWithoutRepeats);
     }
 }
